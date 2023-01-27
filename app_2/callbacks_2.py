3e45b99b6b30c271a3938bf15747a4db01cfaa80
Rah <rah@consult-rah.com>
2023-01-26 20:40:45 -0500

    app_2

--- app_2/callbacks_2.py
+++ app_2/callbacks_2.py
@@ -2,36 +2,47 @@ from dash.dependencies import Input, Output, State
 from app_2 import app
 import plotly.graph_objs as go 
 import pandas as pd
+from numpy import random
 
 # Create a DataFrame from the .csv file:
 #df = pd.read_csv('../data/mpg.csv')
 df = pd.read_csv('/Users/rah/Desktop/github/DASH/dash-project-2/data/mpg.csv')
-#creates a list of column names
-features = df.columns
 
+# Add a random "jitter" to model_year to spread out the plot
+df['year'] = df['model_year'] + random.randint(-4,5,len(df))*0.10
 
 @app.callback(
-    Output('feature-graphic', 'figure'),
-    [Input('xaxis', 'value'),
-     Input('yaxis', 'value')])
-def update_graph(xaxis_name, yaxis_name):
-    return {
+    Output('mpg_line', 'figure'),
+    [Input('mpg_scatter', 'hoverData')])
+def callback_graph(hoverData):
+    v_index = hoverData['points'][0]['pointIndex']
+    fig = {
         'data': [go.Scatter(
-            x=df[xaxis_name],
-            y=df[yaxis_name],
-            text=df['name'],
-            mode='markers',
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
+            x = [0,1],
+            y = [0,60/df.iloc[v_index]['acceleration']],
+            mode='lines',
+            line={'width':2*df.iloc[v_index]['cylinders']}
         )],
         'layout': go.Layout(
-            title= yaxis_name.title()+' vs. '+xaxis_name.title(),
-            xaxis={'title': xaxis_name.title()},
-            yaxis={'title': yaxis_name.title()},
-            margin={'l': 40, 'b': 40, 't': 50, 'r': 0}, #adds margin for the graph
-            hovermode='closest'
+            title = df.iloc[v_index]['name'],
+            xaxis = {'visible':False},
+            yaxis = {'visible':False, 'range':[0,60/df['acceleration'].min()]},
+            margin = {'l':0},
+            height = 300
         )
     }
+    return fig
+
+@app.callback(
+    Output('mpg_stats', 'children'),
+    [Input('mpg_scatter', 'hoverData')])
+def callback_stats(hoverData):
+    v_index = hoverData['points'][0]['pointIndex']
+    stats = """
+        {} cylinders
+        {}cc displacement
+        0 to 60mph in {} seconds
+        """.format(df.iloc[v_index]['cylinders'],
+            df.iloc[v_index]['displacement'],
+            df.iloc[v_index]['acceleration'])
+    return stats
--- app_2/layout_2.py
+++ app_2/layout_2.py
@@ -1,32 +1,55 @@
 import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
+from numpy import random
+import plotly.graph_objs as go
 
 # Create a DataFrame from the .csv file:
 #df = pd.read_csv('../data/mpg.csv')
 df = pd.read_csv('/Users/rah/Desktop/github/DASH/dash-project-2/data/mpg.csv')
-#creates a list of column names
-features = df.columns
 
-# Create a Dash layout that contains a Graph component:
-layout = html.Div([ # app layout has three dcc components > 2 dropdowns and 1 graph > division takes in a list
+# Add a random "jitter" to model_year to spread out the plot
+df['year'] = df['model_year'] + random.randint(-4,5,len(df))*0.10
 
-    html.Div([
-        dcc.Dropdown(
-            id='xaxis',
-            options=[{'label': i.title(), 'value': i} for i in features],
-            value='displacement'
-        )
-    ],
-    style={'width': '48%', 'display': 'inline-block'}),
-
-    html.Div([
-        dcc.Dropdown(
-            id='yaxis',
-            options=[{'label': i.title(), 'value': i} for i in features],
-            value='acceleration'
-        )
-    ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
-
-dcc.Graph(id='feature-graphic')
-], style={'padding':30})
+layout = html.Div([
+    html.Div([   # this Div contains our scatter plot
+    dcc.Graph(
+        id='mpg_scatter',
+        figure={
+            'data': [go.Scatter(
+                x = df['year']+1900,  # our "jittered" data
+                y = df['mpg'],
+                text = df['name'],
+                hoverinfo = 'text',
+                mode = 'markers'
+            )],
+            'layout': go.Layout(
+                title = 'mpg by model & year of make',
+                xaxis = {'title': 'model year'},
+                yaxis = {'title': 'miles per gallon'},
+                hovermode='closest'
+            )
+        }
+    )], style={'width':'50%','display':'inline-block'}),
+    html.Div([  # this Div contains our output graph and vehicle stats
+    dcc.Graph(
+        id='mpg_line',
+        figure={
+            'data': [go.Scatter(
+                x = [0,1],
+                y = [0,1],
+                mode = 'lines'
+            )],
+            'layout': go.Layout(
+                title = 'acceleration',
+                xaxis = {'title': 'xaxis'},
+                yaxis = {'title': 'yaxis'},
+                margin = {'l':0}
+            )
+        }
+    ),
+    dcc.Markdown(
+        id='mpg_stats'
+    )
+    ], style={'width':'20%', 'height':'50%','display':'inline-block'})
+])
